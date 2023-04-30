8c180678dc130dd4e94120b5f66128fa94cc4258
unknown <polianabadio@gmail.com>
2023-04-29 22:48:17 -0300

    first commit

---
+++ home.js
@@ -0,0 +1,9 @@
+function mostrar() {
+    const modal = document.getElementById('modal-cooperador');
+    modal.style.display = "block";
+  }
+
+function ocultar () {
+    const modal = document.getElementById('modal-cooperador');
+    modal.style.display = "none";
+  }
